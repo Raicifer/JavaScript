195949e5c670701266908c28c3860462b028793b
Shouvik Guha <101465057+Raicifer@users.noreply.github.com>
2024-04-29 08:33:51 +0000

    datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+console.log(3+3)
+let name="Shouvik" //string 
+let age=21 //number
+let isloggedin=true //boolean
+let bigno = 123456785557688686
+
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof isloggedin)
+console.log(typeof bigno)
+
