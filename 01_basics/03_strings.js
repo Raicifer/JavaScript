473d3c34e34913db0f5839546638b7552a63e200
Shouvik Guha <101465057+Raicifer@users.noreply.github.com>
2024-05-04 06:23:28 +0000

    string

---
+++ 01_basics/03_strings.js
@@ -0,0 +1,19 @@
+let name="Shouvik"
+let repono=5
+// console.log(name + repono + "value")
+console.log(`Hello my name is ${name} and my repo count is ${repono}`)
+
+//console.log(name[0]);
+//console.log(name.length);
+//console.log(name.toUpperCase());
+//console.log(name.charAt(3));
+//console.log(name.indexOf("v"));
+
+const newString="   shouvik   "
+console.log(newString)
+console.log(newString.trim())
+
+const url="https://shouvik.com/shouvik%10guha"
+console.log(url.replace('%10','-'))
+console.log(url.includes('guha'))
+console.log(url.split('/'))
