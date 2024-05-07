794a11fc8cd0b92d6d6cfe54d7f280cd02216dd2
Shouvik Guha <101465057+Raicifer@users.noreply.github.com>
2024-05-07 08:38:23 +0000

    math

--- 01_basics/04numAndMath.js
+++ 01_basics/04numAndMath.js
@@ -2,3 +2,13 @@ let balance=new Number(100)
 console.log(balance)
 console.log(balance.toString().length)
 console.log(balance.toFixed(2))
+const otherno=1503.7112
+console.log(otherno.toPrecision(5))
+
+const num=1000000
+console.log(num.toLocaleString('en-IN'))
+
+// maths //
+console.log(Math.round(4.6))
+console.log(Math.floor(4.6))
+console.log(Math.sqrt(25))
