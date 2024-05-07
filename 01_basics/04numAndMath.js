1afc9f325cddc9ddf9b65e89706d9168b2773a73
Shouvik Guha <101465057+Raicifer@users.noreply.github.com>
2024-05-07 14:50:55 +0000

    math

--- 01_basics/04numAndMath.js
+++ 01_basics/04numAndMath.js
@@ -1,14 +1,26 @@
 let balance=new Number(100)
-console.log(balance)
-console.log(balance.toString().length)
-console.log(balance.toFixed(2))
+//console.log(balance)
+//console.log(balance.toString().length)
+//console.log(balance.toFixed(2))
 const otherno=1503.7112
-console.log(otherno.toPrecision(5))
+//console.log(otherno.toPrecision(5))
 
 const num=1000000
-console.log(num.toLocaleString('en-IN'))
+//console.log(num.toLocaleString('en-IN'))
 
 // maths //
-console.log(Math.round(4.6))
-console.log(Math.floor(4.6))
-console.log(Math.sqrt(25))
+
+//console.log(Math.round(4.6))
+//console.log(Math.floor(4.6))
+//console.log(Math.sqrt(25))
+//console.log(Math.max(7,3,8,4,9))
+//console.log(Math.min(7,3,8,4,9))
+
+console.log(Math.random())
+//console.log(Math.random()*10)
+console.log((Math.random()*10)+1)
+console.log(Math.floor(Math.random()*10)+1)
+const min=2
+const max=7
+console.log(Math.floor(Math.random()*(max-min+1))+min)
+
