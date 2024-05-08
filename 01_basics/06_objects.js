6fd957c7afede49d9e459c86280917194c560c39
Shouvik Guha <101465057+Raicifer@users.noreply.github.com>
2024-05-08 16:18:36 +0000

    com

--- 01_basics/06_objects.js
+++ 01_basics/06_objects.js
@@ -5,5 +5,21 @@ const User={
     email:"shouvik@gmail.com"
 
 }
+User.email="shouvikguha@gmail.com"
+//console.log(User.email)
 
-console.log(User.email)
+User.greeting=function(){
+    console.log("HELLO")
+}
+User.greeting2=function(){
+    console.log(`hello ${this.name}`)
+}
+//console.log(User.greeting())
+//console.log(User.greeting2())
+
+const ob1={1:"a",2:"b"}
+const ob2={3:"a",4:"b"}
+const ob3={5:"a",6:"b"}
+
+const ob={...ob1,...ob2,...ob3}
+console.log(ob)
