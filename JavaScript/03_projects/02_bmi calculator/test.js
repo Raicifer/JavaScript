84ff607872611e1e4a4f2309b0a432d5cd401172
Shouvik Guha <101465057+Raicifer@users.noreply.github.com>
2024-06-28 09:00:23 +0000

    ok

--- JavaScript/03_projects/02_bmi calculator/test.js
+++ JavaScript/03_projects/02_bmi calculator/test.js
@@ -1,4 +1,6 @@
 const form = document.querySelector('form');
+// this usecase will give you empty
+// const height = parseInt(document.querySelector('#height').value)
 
 form.addEventListener('submit', function (e) {
   e.preventDefault();
@@ -8,11 +10,13 @@ form.addEventListener('submit', function (e) {
   const results = document.querySelector('#results');
 
   if (height === '' || height < 0 || isNaN(height)) {
-    results.innerHTML=`Please give a valid height ${height}`;
-  } else if (weight === '' || weight <0 || isNaN(weight)){
-    results.innerHTML= `Please give a valid weight ${weight}`;
+    results.innerHTML = `Please give a valid height ${height}`;
+  } else if (weight === '' || weight < 0 || isNaN(weight)) {
+    results.innerHTML = `Please give a valid weight ${weight}`;
   } else {
     const bmi = (weight / ((height * height) / 10000)).toFixed(2);
-    results.innerHTML= `<span>${bmi}</span>`;
+    //show the result
+    results.innerHTML = `<span>${bmi}</span>`;
   }
 });
+
